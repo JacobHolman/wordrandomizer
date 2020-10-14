3ae14dda855645a27c1b2ff51e367959e3c6aeff
LavaAdventures <69821200+LavaAdventures@users.noreply.github.com>
2020-10-14 19:13:23 -0400

    Create project.js
    I have gotten this far can not fix out how to get number of the word its on like "hey" = 1 "hello" = 2 "die" = 3 "you" = 4 "killed" = 5 "me" = 6 "pro" = 7 "games" = 8

---
+++ project.js
@@ -0,0 +1,4 @@
+var words = ['hey', 'hello', 'die', 'you', 'killed', 'me', 'pro', 'games', 'code', 'editor', 'sucks', 'fly', 'flyed', 'ok', 'okay', 'alright', 'hi', 'nice', 'html', 'js', 'css'];
+var wordsnumber = 0
+var getnumber = wordsnumber - words
+document.getElementById('word').innerText = (words);
